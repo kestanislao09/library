c625d08b0912004dd903348068e319aeb6c4dc7d
Keith Estanislao <kestanislao09@outlook.com>
2022-03-10 04:26:45 -0800

    Form Modal done, buttons done.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,108 +1,120 @@
 // Array to hold all of the books
 let libraryArr = []
 
+// Factory function for books.
 const Book = (title, author, pages, type, read, nsfw) => {
-
-    const createHeader = () => {
-        const cardHead = document.createElement('div');
-        cardHead.classList.toggle('card-header');
-        cardHead.textContent = this.title;
-        return cardHead;
-    };
-
-    const createDescription = () => {
-        const cardDesc = document.createElement('div');
-        cardDesc.classList.toggle('card-description');
-        cardDesc.textContent = `A ${this.type} by ${this.author}. ${this.pages} Pages.`;
-    };
-
-    const createReadBadge = () => {
-        const readBadge = document.createElement('div');
-        readBadge.classList.toggle('badge');
-        if (this.read === true) {
-            readBadge.classList.toggle('blue');
-            readBadge.textContent = 'Read';
-        } else if ( this.read === false) {
-            readBadge.classList.toggle('orange');
-            readBadge.textContent = 'Unread';
+    // Removes an entry from the library array, then refreshes the displayed cards.
+    const deleteBook = (thisBook) => {
+        if (thisBook == 0) {
+            libraryArr.shift();
+        } else if (thisBook != 0) {
+            libraryArr.splice(thisBook, 1);
         };
+
+        
+        clearLibrary();
+        displayLibrary();
     };
 
-    const createNsfwBadge = () => {
-        const nsfwBadge = document.createElement('div');
-        nsfwBadge.classList.toggle('badge');
-        if (this.nsfw === true) {
-            nsfwBadge.classList.toggle('pink');
-            nsfwBadge.textContent = 'NSFW';
-        } else if ( this.nsfw === false) {
-            nsfwBadge.classList.toggle('green');
-            nsfwBadge.textContent = 'SFW';
+    const toggleRead = (thisBook) => {
+        if (libraryArr[thisBook].read === true) {
+            libraryArr[thisBook].read = false;
+        } else if (libraryArr[thisBook].read === false) {
+            libraryArr[thisBook].read = true;
         };
-    };
-
-    const toggleRead = () => {
-        if (this.read === true) {
-            this.read = false;
-        } else if ( this.read === false) {
-            this.read = true;
-        };
-    };
-
     
-    return {title, author, pages, type, read, nsfw, createHeader, createDescription, createReadBadge, createNsfwBadge, toggleRead}
+        clearLibrary();
+        displayLibrary();
+    };
+    
+    return {title, author, pages, type, read, nsfw, deleteBook, toggleRead}
 };
-
 
 function addBook(title, author, pages, type, read, nsfw) {
     let newBook = Book(title, author, pages, type, read, nsfw);
     libraryArr.push(newBook);
 }
 
-function deleteBook(thisBook) {
-    if (thisBook === 0) {
-        libraryArr.shift();
-    } else if (thisBook !== 0) {
-        libraryArr.splice(thisBook - 1, 1);
-    };
 
+// Modal stuff
+const modal = document.querySelector('.modal');
+const addBtn = document.querySelector('.add-btn');
+const closeBtn = document.querySelector('.close-btn');
+const submitBtn = document.querySelector('.submit-btn');
+
+addBtn.addEventListener('click', e => {
+    modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', e => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', e => {
+    if (e.target == modal) {
+        modal.style.display = 'none'
+    }
+});
+
+submitBtn.addEventListener('click', e => {
+    const formTitle = document.querySelector('#title');
+    const formAuthor = document.querySelector('#author');
+    const formPages = document.querySelector('#pages');
+    const formRead = document.querySelector('#read');
+    const formNsfw = document.querySelector('#nsfw');
+    const formType = document.querySelectorAll('input[name="type"]');
+
+    let litType;
+    for(i = 0; i < formType.length; i++) {
+        if (formType[i].checked) {
+            litType = formType[i].value
+        }
+    }
+
+    addBook(formTitle.value, formAuthor.value, formPages.value, litType, formRead.checked, formNsfw.checked)
     clearLibrary();
     displayLibrary();
-};
+    modal.style.display = 'none'
+})
 
-addBook('samplebook1', 'author', '69', 'book', true, false)
-addBook('samplebook2', 'author', '69', 'book', false, true)
-addBook('samplebook3', 'author', '69', 'book', false, false)
 
+
+// Loops through the container and removes all children elements.
 function clearLibrary() {
     const container = document.querySelector('.container');
     let book = container.firstElementChild;
-
+    
     while(book) {
         container.removeChild(book);
         book = container.firstElementChild;
     };
 };
 
+// Loops through each entry in the library to make cards and add them to the display
 function displayLibrary() {
     libraryArr.forEach(book => {
         const container = document.querySelector('.container');
         const card = document.createElement('div');
-        const deleteBtn = document.createElement('button');
         const cardHead = document.createElement('div')
         const cardDesc = document.createElement('div');
+        const badges = document.createElement('div');
         const readBadge = document.createElement('div');
         const nsfwBadge = document.createElement('div');
-
+        const deleteBtn = document.createElement('button');
+        const readBtn = document.createElement('button');
+        
         // Creates the card header
         cardHead.classList.toggle('card-header');
         cardHead.textContent = book.title;
-
+        
         // Creates the card description
         cardDesc.classList.toggle('card-description');
         cardDesc.textContent = `A ${book.type} by ${book.author}. ${book.pages} Pages.`;
-
+        
         // Creates the two badges
+        badges.classList.toggle('badges')
         readBadge.classList.toggle('badge');
+        readBadge.classList.toggle('readBadge');
         if (book.read === true) {
             readBadge.classList.toggle('blue');
             readBadge.textContent = 'Read';
@@ -110,7 +122,9 @@ function displayLibrary() {
             readBadge.classList.toggle('orange');
             readBadge.textContent = 'Unread';
         };
-
+        
+        nsfwBadge.classList.toggle('badge');
+        nsfwBadge.classList.toggle('nsfwBadge');
         if (book.nsfw === true) {
             nsfwBadge.classList.toggle('pink');
             nsfwBadge.textContent = 'NSFW';
@@ -118,24 +132,38 @@ function displayLibrary() {
             nsfwBadge.classList.toggle('green');
             nsfwBadge.textContent = 'SFW';
         };
-
+        
         // Creates the delete button
         deleteBtn.classList.toggle('delete-btn');
-        deleteBtn.textContent = 'Delete'
+        deleteBtn.setAttribute('data-index', `${libraryArr.indexOf(book)}`);
+        deleteBtn.textContent = 'Del'
         deleteBtn.addEventListener('click', e => {
-            deleteBook(libraryArr.indexOf(book));
+            libraryArr[e.target.getAttribute('data-index')].deleteBook(parseInt(e.target.getAttribute('data-index')));
         })
         
+        // Creates the read toggle button
+        readBtn.classList.toggle('read-btn');
+        readBtn.setAttribute('data-index', `${libraryArr.indexOf(book)}`);
+        readBtn.textContent = 'R'
+        readBtn.addEventListener('click', e => {
+            libraryArr[e.target.getAttribute('data-index')].toggleRead(parseInt(e.target.getAttribute('data-index')));
+        })
+        
+        // Appends all of the parts of the card, then adds the card to the container.
         card.classList.toggle('book-card');
-        card.setAttribute('id', `${libraryArr.indexOf(book)}`);
         card.appendChild(cardHead);
         card.appendChild(cardDesc);
-        card.appendChild(readBadge);
-        card.appendChild(nsfwBadge);
+        card.appendChild(badges)
+        badges.appendChild(readBadge);
+        badges.appendChild(nsfwBadge);
+        card.appendChild(readBtn)
         card.appendChild(deleteBtn);
         
         container.appendChild(card);
     });
 };
 
+addBook('samplebook1', 'author', '69', 'book', true, false)
+addBook('samplebook2', 'author', '69', 'book', false, true)
+addBook('samplebook3', 'author', '69', 'book', false, false)
 displayLibrary();
